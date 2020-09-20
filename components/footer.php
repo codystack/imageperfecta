82e74efd9510aacf447c034f6fee0e9e559183e1
codystack <ebuzzadvert@gmail.com>
2020-09-20 15:52:30 +0100

    V1

---
+++ components/footer.php
@@ -0,0 +1,48 @@
+<footer class="site-footer">
+		<div class="container">
+
+			<div class="row mb-1">
+				<p class="col-12 text-center">
+
+					Copyright &copy; <script>document.write(new Date().getFullYear());</script> All Rights Reserved | Built with <i class="icon-heart text-danger" aria-hidden="true" ></i> by <a href="https://thankgodokoro.codes" target="_blank" stlye="color: #fff;">TeeGee</a>
+
+				</p>
+			</div>
+			
+			<!--<div class="row mb-5">
+				<div class="col-md-12 text-center">
+					<p>
+						<a href="#" class="social-item"><span class="icon-facebook2"></span></a>
+						<a href="#" class="social-item"><span class="icon-twitter"></span></a>
+						<a href="#" class="social-item"><span class="icon-instagram2"></span></a>
+						<a href="#" class="social-item"><span class="icon-linkedin2"></span></a>
+					</p>
+				</div>
+			</div>-->
+			
+		</div>
+	</footer>
+
+
+
+
+	<script src="js/vendor/jquery.min.js"></script>
+	<script src="js/vendor/jquery-migrate-3.0.1.min.js"></script>
+	<script src="js/vendor/popper.min.js"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+
+	<script src="js/vendor/jquery.easing.1.3.js"></script>
+
+	<script src="js/vendor/jquery.stellar.min.js"></script>
+	<script src="js/vendor/jquery.waypoints.min.js"></script>
+
+	<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+	<script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+	<script src="js/custom.js"></script>
+
+	<!-- Google Map -->
+    <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    	<script src="js/google-map.js"></script> -->
+
+    </body>
+    </html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+include ('./components/header.php');
+?>
+
+
+
+
+
+
+	<section style="background: #fff;">
+		<div class="container">
+			<div class="row">
+				<div class="section-heading text-center col-md-12" style="margin-top: 50px;">
+					<img src="./images/logo.png" style="width: 250px;">
+				</div>
+			</div>
+
+			<div class="filters-content" style="margin-top: 50px;">
+				<div class="row grid">
+					<div class="single-portfolio col-sm-4 all mockup">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image" src="./images/8.jpg" alt="">
+							</div>                                 
+						</div>
+						<div class="p-inner">
+							<h4>Square Box Mockup</h4>
+						</div>                                         
+					</div>
+					<div class="single-portfolio col-sm-4 all mockup">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image img-fluid" src="images/p2.jpg" alt="">
+							</div>
+							<a href="images/p2.jpg" class="img-pop-up">  
+								<div class="middle">
+									<div class="text align-self-center d-flex"><img src="images/preview.png" alt=""></div>
+								</div>
+							</a>                                  
+						</div>
+						<div class="p-inner">
+							<h4>Product Box Package Mockup</h4>
+						</div>                                         
+					</div>                            
+					<div class="single-portfolio col-sm-4 all packaging">
+						<div class="relative">
+							<div class="thumb">
+								<div class="overlay overlay-bg"></div>
+								<img class="image img-fluid" src="images/p3.jpg" alt="">
+							</div>
+							<a href="images/p3.jpg" class="img-pop-up">  
+								<div class="middle">
+									<div class="text align-self-center d-flex"><img src="images/preview.png" alt=""></div>
+								</div>
+							</a> 
+
+						</div>
+						<div class="p-inner">
+							<h4>Creative Package Design</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- .section -->
+
+
+
+<?php include ('./components/footer.php'); ?>
